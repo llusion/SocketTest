ddbc7674837eff35db0fd8e72a4f5950d6799156
ls <activity0@sina.com>
2019-04-12 14:59:45 +0800

    加注释

--- app/src/main/java/com/app/jssl/sockettest/utils/SocketUtils.java
+++ app/src/main/java/com/app/jssl/sockettest/utils/SocketUtils.java
@@ -1,8 +1,10 @@
 package com.app.jssl.sockettest.utils;
 
 import android.content.Context;
+import android.os.Handler;
 
 import com.app.jssl.sockettest.eventbus.LoginEvent;
+import com.app.jssl.sockettest.eventbus.SocketEvent;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -23,6 +25,8 @@ public class SocketUtils {
     public static Context activity;
     public static OutputStream outputStream;
     public static InputStream inputStream;
+    public static Handler handler;
+    public static Runnable runnable;
 
     private SocketUtils(Context activity) {
         SocketUtils.activity = activity;
@@ -44,12 +48,16 @@ public class SocketUtils {
                         outputStream = socket.getOutputStream();
                         inputStream = socket.getInputStream();
                         EventBus.getDefault().post(new LoginEvent(Time.now(), true, "socket连接成功", "连接"));
+                        EventBus.getDefault().post(new SocketEvent(Time.now(), "socket连接成功"));
                     } catch (UnknownHostException e) {
                         EventBus.getDefault().post(new LoginEvent(Time.now(), false, "socket连接失败", "连接"));
+                        EventBus.getDefault().post(new SocketEvent(Time.now(), "socket连接失败"));
                     } catch (IOException e) {
                         EventBus.getDefault().post(new LoginEvent(Time.now(), false, "socket连接失败", "连接"));
+                        EventBus.getDefault().post(new SocketEvent(Time.now(), "socket连接失败"));
                     } catch (NullPointerException e) {
                         EventBus.getDefault().post(new LoginEvent(Time.now(), false, "socket连接失败", "连接"));
+                        EventBus.getDefault().post(new SocketEvent(Time.now(), "socket连接失败"));
                     }
                 }
             }
@@ -76,6 +84,28 @@ public class SocketUtils {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void handleException() {
+        SocketUtils.release();
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (SocketUtils.socket == null) {
+                    //因服务器端是只接受一个socket连接，所以需要等待端口释放才能连上
+                    //实际情况都是多线程的，可以直接新建socket连接。
+                    SocketUtils.getSocket();
+                } else {
+                    handler.removeCallbacks(this::run);
+                }
+                handler.postDelayed(runnable, 120 * 1000);
+            }
+        };
+        handler.post(runnable);
+        if (SocketUtils.socket != null) {
+            handler.removeCallbacks(runnable);
         }
     }
 }
